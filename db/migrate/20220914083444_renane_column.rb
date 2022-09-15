f6708143564eb1f2fbb0ebe95facbd91315f82ad
ArnaudBand <bandonkeyea@gmail.com>
2022-09-15 10:25:31 -0600

    Delete the file rename

--- db/migrate/20220914083444_renane_column.rb
+++
@@ -1,7 +0,0 @@
-class RenaneColumn < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :comments, :user_id, :user
-    rename_column :posts, :user_id, :user
-    rename_column :likes, :user_id, :user
-  end
-end
