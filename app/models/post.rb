d39cc811407202dbc58474a2d7ff94d994e0c802
ArnaudBand <bandonkeyea@gmail.com>
2022-09-16 15:50:00 -0600

    Add validation and initiate some elements to zero

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, :text, presence: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
   after_save :update_counters
+
+  after_initialize do |post|
+    post.comments_counter = 0
+    post.likes_counter = 0
+  end
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -12,6 +21,6 @@ class Post < ApplicationRecord
   private
 
   def update_counters
-    author.increment!(:postsCounter)
+    author.increment!(:posts_counter)
   end
 end
