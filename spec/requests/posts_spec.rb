9cd727368a78db2276867317a2b1cc128289240a
ArnaudBand <bandonkeyea@gmail.com>
2022-09-20 10:51:54 -0600

    Add user as author and create the test for Post "show page"

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "Posts", type: :request do
   let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  subject(:post) { Post.create(author:, title: 'Hello 1', text: 'This is my first post')}
+  subject(:post) { Post.create(author: user, title: 'Hello 1', text: 'This is my first post')}
 
   describe 'GET /posts' do
     before(:example) do
@@ -19,6 +19,24 @@ RSpec.describe "Posts", type: :request do
 
     it 'has the correct placeholder text' do
       expect(response.body).to include('Here are a list of all the posts')
+    end
+  end
+
+  describe 'GET /posts/:id' do
+    before(:example) do
+      get "#{users_path}/#{user.id}/posts/#{post.id}"
+    end
+
+    it 'should succeed to render show' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Renders the correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'has the correct placeholder text' do
+      expect(response.body).to include("Here is the post with id")
     end
   end
 end
