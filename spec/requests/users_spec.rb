65cc6190de656ee1270b6fd1f959fab0945f05c0
ArnaudBand <bandonkeyea@gmail.com>
2022-09-20 10:37:19 -0600

    Create a test for show page

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe UsersController, type: :request do
       expect(response.body).to include('Here are a list of all the users')
     end
   end
+
+  describe 'GET user#show' do
+    subject(:user) {User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
+
+    before(:example) do
+      get "#{users_path}/#{user.id}"
+    end
+
+    it 'should succeed to render show' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Renders the correct template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'has the correct placeholder text' do
+      expect(response.body).to include('Here is the user with id')
+    end
+  end
 end
