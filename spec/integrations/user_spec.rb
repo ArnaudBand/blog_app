18117756b2a246ec6667c2047ee2ad6f521195e7
toMilanesi <tomi.milanesi@gmail.com>
2022-09-27 17:40:11 -0300

    Added test to user#show

--- spec/integrations/user_spec.rb
+++ spec/integrations/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-
   before(:all) do
     Rails.application.load_seed
   end
@@ -38,4 +37,30 @@ RSpec.describe 'Users', type: :system do
     end
   end
 
+  describe 'show page:' do
+    before(:example) { visit user_path(2) }
+
+    it 'should show profle picture' do
+      image = page.all('img')
+      expect(image.size).to eq(3)
+    end
+
+    it 'should show users name' do
+      expect(page).to have_content('Tom')
+    end
+
+    it 'should show the numbre of posts' do
+      expect(page).to have_content('Number of posts: 4')
+    end
+
+    # it 'sholud show the Bio' do
+    #   expect(page).to have_content('Bio')
+    # end
+
+    it 'should show first 3 posts' do
+      expect(page).to have_content('Hello 4')
+      expect(page).to have_content('Hello 3')
+      expect(page).to have_content('Hello 2')
+    end
+  end
 end
